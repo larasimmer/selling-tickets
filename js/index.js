7132162a69c26e09a63f18ceca1f689a0c5da0d6
Lara Simmer <larafsimmer@gmail.com>
2021-12-18 19:26:57 -0300

    Subindo arquivo JS

---
+++ js/index.js
@@ -0,0 +1,3 @@
+let inputSelecaoQuantidadeIngressos = document.getElementsByClassName("event__tickets-select");
+let quantidadeIngressosInteirosNoResumo = document.getElementById("quantidade-ingressos-inteiros");
+let quantidadeIngressosMeiaNoResumo = document.getElementById("quantidade-ingressos-meia");
