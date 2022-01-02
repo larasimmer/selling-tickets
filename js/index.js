33b59f43f5856fb6c4885b770944eb0f871b6a78
Lara Simmer <larafsimmer@gmail.com>
2022-01-02 16:24:46 -0300

    Refatorando HTML e SCSS

--- js/index.js
+++
@@ -1,3 +0,0 @@
-let inputSelecaoQuantidadeIngressos = document.getElementsByClassName("event__tickets-select");
-let quantidadeIngressosInteirosNoResumo = document.getElementById("quantidade-ingressos-inteiros");
-let quantidadeIngressosMeiaNoResumo = document.getElementById("quantidade-ingressos-meia");
