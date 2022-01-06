e659b094afb24a7016d33d111d09a4d2c66c7aef
Lara Simmer <larafsimmer@gmail.com>
2022-01-06 13:46:01 -0300

    Subindo atualizações nas validaçẽos do formulário de pagamento

--- js/validacoes-informacoes-pagamento.js
+++ js/validacoes-informacoes-pagamento.js
@@ -1,6 +1,6 @@
 let inputNumeroDoCartao = document.getElementById("numero-cartao");
 let erroNumeroDoCartaoVazio = document.getElementById("numero-cartao-vazio");
-let inputAnoDeValidadeCartao = document.getElementById("ano-validade");
+let inputAnoDeValidade= document.getElementById("ano-validade");
 let erroAnoDeValidadeVazio = document.getElementById("ano-validade-vazio");
 let inputCodigoDeSeguranca = document.getElementById("codigo-seguranca");
 let erroCodigoDeSegurancaVazio = document.getElementById("codigo-seguranca-vazio");
@@ -12,3 +12,53 @@ let erroDataDeNascimentoInvalida = document.getElementById("nascimento-invalido"
 let inputCpf = document.getElementById("cpf");
 let erroCpfVazio = document.getElementById("cpf-vazio");
 let erroCpfInvalido = document.getElementById("cpf-invalido");
+let inputTelefone = document.getElementById("telefone");
+let erroTelefoneVazio = document.getElementById("telefone-vazio");
+let inputCep = document.getElementById("cep");
+let erroCepVazio = document.getElementById("cep-vazio");
+let inputLogradouro = document.getElementById("logradouro");
+let erroLogradouroVazio = document.getElementById("logradouro-vazio");
+let inputBairro = document.getElementById("bairro");
+let erroBairroVazio = document.getElementById("bairro-vazio");
+let inputCidade = document.getElementById("cidade");
+let erroCidadeVazia = document.getElementById("cidade-vazio");
+
+let formularioDePagamento = document.getElementById("form-pagamento");
+let botaoFinalizar = document.getElementById("botao-finalizar");
+
+botaoFinalizar.onclick = realizaValidacoes;
+
+function realizaValidacoes() {
+    validaNumeroDoCartaoVazio();
+}
+
+function validaNumeroDoCartaoVazio() {
+    let valorNumeroDoCartao = inputNumeroDoCartao.value;
+    let numeroDoCartaoVazio = false;
+
+    if (valorNumeroDoCartao.length <= 0) {
+        numeroDoCartaoVazio = true;
+        erroNumeroDoCartaoVazio.style.display = "block";
+    } else {
+        erroNumeroDoCartaoVazio.style.display = "none";
+        numeroDoCartaoVazio = false;
+    }
+
+    return numeroDoCartaoVazio;
+}
+
+function validaAnoDeValidadeVazio() {
+    let valorAnoDeValidade = inputAnoDeValidade.value;
+    let anoDeValidadeVazio = false;
+
+    if (valorAnoDeValidade.length <= 0) {
+        anoDeValidadeVazio = true;
+        erroAnoDeValidadeVazio.style.display = "block";
+    } else {
+        erroAnoDeValidadeVazio.style.display = "none";
+        anoDeValidadeVazio = false;
+    }
+
+    return anoDeValidadeVazio;
+}
+
