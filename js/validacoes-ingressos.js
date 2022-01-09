c8d2df19f532a976f64f5414a71c747b8ef93131
Lara Simmer <larafsimmer@gmail.com>
2022-01-09 09:09:29 -0300

    Preço total dinâmico no resumo

--- js/validacoes-ingressos.js
+++ js/validacoes-ingressos.js
@@ -1,89 +1,113 @@
 let listaDoResumo = document.getElementById("resumo");
 let precoTotal = document.getElementById("total");
-precoTotal = 0;
+let total = 0;
 
 $("#inteira-angra").change(function(){
     let quantidade = $(this).val();
     let tipo = $(this).data("tipo");
     let nome = $(this).data("nome");
+    let preco = $(this).data("preco");
     adicionaLinhaNoResumo(quantidade, tipo, nome);
+    adicionaPrecoAoTotal(quantidade, preco);
 });
 
 $("#meia-angra").change(function(){
     let quantidade = $(this).val();
     let tipo = $(this).data("tipo");
     let nome = $(this).data("nome");
+    let preco = $(this).data("preco");
     adicionaLinhaNoResumo(quantidade, tipo, nome);
+    adicionaPrecoAoTotal(quantidade, preco);
 });
 
 $("#inteira-lana").change(function(){
     let quantidade = $(this).val();
     let tipo = $(this).data("tipo");
     let nome = $(this).data("nome");
+    let preco = $(this).data("preco");
     adicionaLinhaNoResumo(quantidade, tipo, nome);
+    adicionaPrecoAoTotal(quantidade, preco);
 });
 
 $("#meia-lana").change(function(){
     let quantidade = $(this).val();
     let tipo = $(this).data("tipo");
     let nome = $(this).data("nome");
+    let preco = $(this).data("preco");
     adicionaLinhaNoResumo(quantidade, tipo, nome);
+    adicionaPrecoAoTotal(quantidade, preco);
 });
 
 $("#inteira-ozzy").change(function(){
     let quantidade = $(this).val();
     let tipo = $(this).data("tipo");
     let nome = $(this).data("nome");
+    let preco = $(this).data("preco");
     adicionaLinhaNoResumo(quantidade, tipo, nome);
+    adicionaPrecoAoTotal(quantidade, preco);
 });
 
 $("#meia-ozzy").change(function(){
     let quantidade = $(this).val();
     let tipo = $(this).data("tipo");
     let nome = $(this).data("nome");
+    let preco = $(this).data("preco");
     adicionaLinhaNoResumo(quantidade, tipo, nome);
+    adicionaPrecoAoTotal(quantidade, preco);
 });
 
 $("#inteira-beyonce").change(function(){
     let quantidade = $(this).val();
     let tipo = $(this).data("tipo");
     let nome = $(this).data("nome");
+    let preco = $(this).data("preco");
     adicionaLinhaNoResumo(quantidade, tipo, nome);
+    adicionaPrecoAoTotal(quantidade, preco);
 });
 
 $("#meia-beyonce").change(function(){
     let quantidade = $(this).val();
     let tipo = $(this).data("tipo");
     let nome = $(this).data("nome");
+    let preco = $(this).data("preco");
     adicionaLinhaNoResumo(quantidade, tipo, nome);
+    adicionaPrecoAoTotal(quantidade, preco);
 });
 
 $("#inteira-theweeknd").change(function(){
     let quantidade = $(this).val();
     let tipo = $(this).data("tipo");
     let nome = $(this).data("nome");
+    let preco = $(this).data("preco");
     adicionaLinhaNoResumo(quantidade, tipo, nome);
+    adicionaPrecoAoTotal(quantidade, preco);
 });
 
 $("#meia-theweeknd").change(function(){
     let quantidade = $(this).val();
     let tipo = $(this).data("tipo");
     let nome = $(this).data("nome");
+    let preco = $(this).data("preco");
     adicionaLinhaNoResumo(quantidade, tipo, nome);
+    adicionaPrecoAoTotal(quantidade, preco);
 });
 
 $("#inteira-rammstein").change(function(){
     let quantidade = $(this).val();
     let tipo = $(this).data("tipo");
     let nome = $(this).data("nome");
+    let preco = $(this).data("preco");
     adicionaLinhaNoResumo(quantidade, tipo, nome);
+    adicionaPrecoAoTotal(quantidade, preco);
 });
 
 $("#meia-rammstein").change(function(){
     let quantidade = $(this).val();
     let tipo = $(this).data("tipo");
     let nome = $(this).data("nome");
+    let preco = $(this).data("preco");
     adicionaLinhaNoResumo(quantidade, tipo, nome);
+    adicionaPrecoAoTotal(quantidade, preco);
 });
 
 function adicionaLinhaNoResumo(quantidadeDeIngresso, tipoDeIngresso, nomeDoEvento) {
@@ -91,6 +115,12 @@ function adicionaLinhaNoResumo(quantidadeDeIngresso, tipoDeIngresso, nomeDoEvent
     li.appendChild(document.createTextNode(`${quantidadeDeIngresso} ingresso(s) ${tipoDeIngresso} para ${nomeDoEvento}`));
     listaDoResumo.appendChild(li);
     li.classList.add("resumo__lista-item");
+}
+
+function adicionaPrecoAoTotal(quantidadeDeIngresso,precoDoIngresso) {
+    let precoASerAdicionadoAoTotal = quantidadeDeIngresso*precoDoIngresso;
+    total = total + precoASerAdicionadoAoTotal;
+    precoTotal.innerHTML = `Total: R$ ${total},00`;
 }
 
 
