1a9adfe46cf18c2deac5797e58c50fb6fd5435f2
Lara Simmer <larafsimmer@gmail.com>
2022-01-09 10:15:33 -0300

    Preço total dinâmico no resumo

--- js/validacoes-ingressos.js
+++ js/validacoes-ingressos.js
@@ -1,5 +1,6 @@
 let listaDoResumo = document.getElementById("resumo");
 let precoTotal = document.getElementById("total");
+let filtro = document.getElementById("busca");
 let total = 0;
 
 $("#inteira-angra").change(function(){
@@ -122,6 +123,29 @@ function adicionaPrecoAoTotal(quantidadeDeIngresso,precoDoIngresso) {
     total = total + precoASerAdicionadoAoTotal;
     precoTotal.innerHTML = `Total: R$ ${total},00`;
 }
+
+filtro.addEventListener("input", function() {
+    let eventos = document.querySelectorAll(".main__lista--item");
+
+    if (this.value.length > 0) {
+        for (let i = 0; i < eventos.length; i++) {
+            let nomeDoEvento = document.querySelector(".evento__titulo").textContent;
+            let evento = eventos[i];
+    
+            if (nomeDoEvento != filtro.value) {
+                evento.style.display = "none";
+            } else {
+                evento.style.display = "block";
+            }
+        }
+  
+    } else {
+        for (let i = 0; i < eventos.length; i++) {
+            evento = eventos[i];
+            evento.style.display = "inline-block";
+        }
+    }
+});
 
 
 
